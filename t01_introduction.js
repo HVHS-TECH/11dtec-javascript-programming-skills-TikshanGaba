230693f0ae1855cf8640e745e64a9efd4e7feba8
TikshanGaba <25371tg@hvhs.school.nz>
2025-04-28 21:46:39 +0000

    Apr 28, 2025, 9:46 PM

--- t01_introduction.js
+++ t01_introduction.js
@@ -1,5 +1,8 @@
-/**************************** 
- Javascript introduction
+/****************************
+ Name of task: Intro to Java Script
+ Name: Tikshan
+ Date: Term two week one
+ Purpose:  Learning javascript
 *****************************/
 console.log("Running t01_introduction.js");
 
