326a5cf05f8a0c8d7fdac2e930694c5b66b739b7
TikshanGaba <25371tg@hvhs.school.nz>
2025-04-30 00:29:14 +0000

    Apr 30, 2025, 12:29 AM

--- t02_variables.js
+++ t02_variables.js
@@ -1,8 +1,8 @@
 /****************************
- Name of task: Intro to Java Script
+ Name of task: Variables
  Name: Tikshan
  Date: Term two week one 2025
- Purpose:  Learning javascript
+ Purpose: Learning Variables in javascript
 *****************************/
 
 
