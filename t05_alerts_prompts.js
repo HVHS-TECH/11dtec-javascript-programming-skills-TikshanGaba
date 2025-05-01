07216cd5ceb9287569465340b7836e15fea2622a
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-01 01:10:45 +0000

    May 1, 2025, 1:10 AM

---
+++ t05_alerts_prompts.js
@@ -0,0 +1,21 @@
+/****************************
+ Name of task: Intro to Java Script
+ Name: Tikshan
+ Date: Term two week one 2025
+ Purpose:  Learning javascript
+*****************************/
+
+
+//Variables
+
+
+/****************************
+Main Code
+****************************/
+
+
+
+
+/**************************** 
+Functions
+****************************/
