caaa221f975d54faedcbdbb91197c357d7c8f864
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-01 00:52:24 +0000

    May 1, 2025, 12:52 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,5 +1,5 @@
 /****************************
- Name of task: Maths
+ Name of task: Variables
  Name: Tikshan
  Date: Term two week one 2025
  Purpose: Learning Variables in Javascript
@@ -9,15 +9,23 @@
 //Variables
 
 
-
-
+var username = "Tikshan";
+var userage = 16;
+var pocketMoney = 20;
+var currentyear = 2025;
+var birthyear = 2008;
 
 /****************************
 Main Code
 ****************************/
 
 
-console.log(1+2);
+console.log("Hi", username)
+console.log("as of", currentyear)
+console.log("you are", userage)
+console.log("you were born in", birthyear)
+console.log("in 10 years you will be",)
+console.log("you have", pocketMoney, "dollars")
 
 
 
