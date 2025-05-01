888bef20d0fe52c94c658d7469d7dc63c944c1b9
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-01 01:28:14 +0000

    May 1, 2025, 1:28 AM

--- t05_alerts_prompts.js
+++ t05_alerts_prompts.js
@@ -16,7 +16,7 @@ alert("Welcome to my program" + userName + "\nPlease enter your name")
 var userName = prompt("What is your name")
 var userage = prompt("What is your age ?")
 var pocketMoney = prompt("how much pocket money do you have ?")
-var equation = prompt ("What is 1 + 1 ?") 
+var equation = prompt ("Guess your age in upcoming 10 years") 
 /**************************** 
 Functions
 ****************************/
