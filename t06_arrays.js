1f843e4d003d7a6c05badc22a980e6c523bbc2d1
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-02 02:40:27 +0000

    May 2, 2025, 2:40 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -13,8 +13,7 @@ let choice = prompt("Pick a number from 0 to 4");
 /****************************
 Main Code
 ****************************/
-alert("Welcome to my program \nWho are you from my class ?")
-alert("Person"+ choice + " is " +classArray[choice])
+alert( choice + classArray[choice] )
 
 /**************************** 
 Functions
