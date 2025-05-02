b19a23766edc292be63b8d2bf7dc0fffbb03c4e5
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-02 02:45:49 +0000

    May 2, 2025, 2:45 AM

--- t06_arrays.js
+++ t06_arrays.js
@@ -7,13 +7,13 @@
 
 
 //Variables
-var userName = alert(" Welcome To my program ");
+var userName = alert(" Welcome To my program, Who are you from my class ?");
 let classArray = ["Tikshan","Adarsh","Yuvi","Lucas","Josh"];
 let choice = prompt("Pick a number from 0 to 4");
 /****************************
 Main Code
 ****************************/
-alert( "who are you from my class ?" + choice + classArray[choice] )
+alert( "Pick 0 to 4 to identify yourself" + choice + classArray[choice] )
 
 /**************************** 
 Functions
