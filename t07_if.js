ed135470e4e5d408e3340cd81a9b2f4ede02b42a
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-09 02:21:58 +0000

    May 9, 2025, 2:21 AM

--- t07_if.js
+++ t07_if.js
@@ -1,1 +1,20 @@
-  
+  /****************************
+ Name of task: Intro to conditional statements
+ Name: Tikshan
+ Date: Term two week two 2025
+ Purpose:  Learning connditional statements
+*****************************/
+
+
+//Variables
+var userName = alert(" Welcome To my program, Who are you from my class ?");
+let classArray = ["Tikshan","Adarsh","Yuvi","Lucas","Josh"];
+let choice = prompt("Pick a number from 0 to 4 to identify yourself");
+/****************************
+Main Code
+****************************/
+alert( " Person Number " + choice + " is " + classArray[choice] )
+
+/**************************** 
+Functions
+****************************/
