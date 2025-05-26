18b0325b7514b5ac3ed3a3aed93d7a650c8d0dda
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-26 21:09:17 +0000

    May 26, 2025, 9:09 PM

---
+++ t12_while_loop.js
@@ -0,0 +1,21 @@
+/****************************
+ Name of task: Intro to Java Script
+ Name: Tikshan
+ Date: Term two week one 2025
+ Purpose:  Learning javascript
+*****************************/
+console.log("Yo!");
+
+//Variables
+
+
+/****************************
+Main Code
+****************************/
+
+
+
+
+/**************************** 
+Functions
+****************************/
