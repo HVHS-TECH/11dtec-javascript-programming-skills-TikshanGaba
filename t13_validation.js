1a59344e3a4824765d235af41831ca542c57d507
TikshanGaba <25371tg@hvhs.school.nz>
2025-05-26 21:49:19 +0000

    May 26, 2025, 9:49 PM

---
+++ t13_validation.js
@@ -0,0 +1,26 @@
+/****************************
+ Name of task: Intro to while loop
+ Name: Tikshan
+ Date: Term two week five 2025
+ Purpose:  Learning while loops in javascript
+*****************************/
+
+
+//Variables
+var password = prompt ("Speak 'friend' and enter");
+
+while (password !="friend"){
+    password = prompt("That's not the password. \nSpeak 'friend' and enter");
+}
+
+alert("Welcome friend!")
+/****************************
+Main Code
+****************************/
+
+
+
+
+/**************************** 
+Functions
+****************************/
