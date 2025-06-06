dc82502afa94bf5dc227ecb888b0b7241c87db52
TikshanGaba <25371tg@hvhs.school.nz>
2025-06-06 02:49:22 +0000

    Jun 6, 2025, 2:49 AM

--- t13_validation.js
+++ t13_validation.js
@@ -10,7 +10,6 @@
 const MINAGE = 0;
 const MAXAGE = 100;
 let userName;
-let userNameIsInvalid = true;
 let userAge;
 let userAgeIsInvalid = true;
 
@@ -18,16 +17,7 @@ let userAgeIsInvalid = true;
 Main Code
 ****************************/
 // userName : loop while user's reply is invalid
-while(userNameIsInvalid) {
     userName = prompt("Hi, Please type your name");
-    if(userName == null || userName == "" || userName == " " || isNaN (userName)  ) {
-        alert("Invalid input : Enter a name");
-    }
-    else { // Name is valid, so exit the userName loop by setting while condition loop condition
-        userNameIsInvalid = false;
-    }
-}
-
 // userAge : loop while user's reply is invalid
 while(userAgeIsInvalid) {
     userAge = prompt("Nice name, " + userName + " Please enter your age ?");
