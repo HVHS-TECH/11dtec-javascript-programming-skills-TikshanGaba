0834894b6bc5c1aa05d0ba1aae0d1067c10f8a6f
TikshanGaba <25371tg@hvhs.school.nz>
2025-06-18 00:00:36 +0000

    Jun 18, 2025, 12:00 AM

--- t16_cancel_and_exit.js
+++ t16_cancel_and_exit.js
@@ -23,11 +23,14 @@ Functions
 function start()
  { 
    userName = prompt("What is your name ?")
- // check if user has clicked cancel
+ if ( !isNaN(userName) || userName == null || userName == "" || userName == " " ){
+    userName = prompt("Invalid! You must enter a name")
+    // check if user has clicked cancel
  if (userName == null){
    // end the program
    return;
  }
+}
    userAge = prompt("Nice name " + userName +" How old are you ?")
  if (userAge == null || userAge == "" || userAge == " " || isNaN(userAge) || userAge < MINAGE || userAge > MAXAGE){
     userAge = prompt("Invalid! You must enter you age between " + MINAGE + " and " + MAXAGE)
