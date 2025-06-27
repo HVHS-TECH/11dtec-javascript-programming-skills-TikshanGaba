bd603417fdbbe94d3b50e6693e87e6f180fc20db
TikshanGaba <25371tg@hvhs.school.nz>
2025-06-27 14:54:31 +0000

    Jun 27, 2025, 2:54 PM

--- t07_if.js
+++ t07_if.js
@@ -7,8 +7,18 @@
 
 
 //Variables
+let MINAGE = 12
+let MAXAGE = 19
 var userName = alert(" Welcome To my program, Verify your age");
 let userAge = prompt("Pick a number from 0 to 100 to identify your age");
+if (userAge == null || userAge == "" || userAge == " " || isNaN(userAge) || userAge < MINAGE || userAge > MAXAGE){
+    userAge = prompt("Invalid! You must enter you age between " + MINAGE + " and " + MAXAGE)
+     // check if user has clicked cancel
+ if (userAge == null){
+   // end the program
+   return;
+ }
+}
 /****************************
 Main Code
 ****************************/
